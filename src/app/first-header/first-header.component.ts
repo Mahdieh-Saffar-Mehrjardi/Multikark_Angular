04648609bf76bf1fa9ced5cad3528093b577ec21
mahdiyehsm.2002@gmail.com <mahdiyehsm.2002@gmail.com>
2023-12-06 19:16:09 +0330

    add first header component

---
+++ src/app/first-header/first-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-header',
+  templateUrl: './first-header.component.html',
+  styleUrls: ['./first-header.component.scss']
+})
+export class FirstHeaderComponent {
+
+}
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
 
 const MaterialComponents = [
   CommonModule,
-  MatButtonModule
+  MatToolbarModule,
+  MatButtonModule, 
+  MatIconModule,
+  MatListModule,
+  MatMenuModule
 ];
 
 @NgModule({
